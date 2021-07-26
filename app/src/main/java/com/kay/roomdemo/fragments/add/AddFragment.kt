bc43283796e3543abd2fbb4f32724be6cdd247e2
kay <kayshiro@live.no>
2021-07-26 17:02:31 +0200

    part 2 done

--- app/src/main/java/com/kay/roomdemo/fragments/add/AddFragment.kt
+++ app/src/main/java/com/kay/roomdemo/fragments/add/AddFragment.kt
@@ -1,21 +1,71 @@
 package com.kay.roomdemo.fragments.add
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextUtils
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.kay.roomdemo.R
+import com.kay.roomdemo.data.User
+import com.kay.roomdemo.data.UserViewModel
+import com.kay.roomdemo.databinding.FragmentAddBinding
 
 
 class AddFragment : Fragment() {
 
+    private var _binding: FragmentAddBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var mUserViewModel: UserViewModel
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add, container, false)
+        _binding = FragmentAddBinding.inflate(inflater, container, false)
+
+        mUserViewModel = ViewModelProvider(this).get(UserViewModel::class.java)
+
+        // ADD BUTTON
+        binding.addBtn.setOnClickListener {
+            insertDataToDatabase()
+        }
+
+        return binding.root
     }
 
+    private fun insertDataToDatabase() {
+        val firstName = binding.addFirstNameEt.text.toString()
+        val lastName = binding.addLastNameEt.text.toString()
+        val age = binding.addAgeEt.text
+
+        if(inputCheck(firstName, lastName, age)){
+            // create user object
+            val user = User(0, firstName, lastName, Integer.parseInt(age.toString()))
+            // Add Data to Database
+            mUserViewModel.addUser(user)
+            Toast.makeText(requireContext(),"Successfully added!", Toast.LENGTH_LONG).show()
+            // Navigate back
+            findNavController().navigate(R.id.action_addFragment_to_listFragment)
+
+        }else{
+            Toast.makeText(requireContext(),"Please fill out all fields.", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    // function that checks if the inputfield is empty
+    private fun inputCheck(firstName: String, lastName: String, age: Editable): Boolean{
+        return !(TextUtils.isEmpty(firstName) && TextUtils.isEmpty(lastName) && age.isEmpty())
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
